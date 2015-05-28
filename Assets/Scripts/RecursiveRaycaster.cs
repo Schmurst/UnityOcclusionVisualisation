eafbfae087f53735a16514ddceba94caab8b4574
Sam <samhayhurst@gmail.com>
2015-05-28 14:02:24 +0100

    ray recursion now doable on user button press and addition of tuple class.

--- Assets/Scripts/RecursiveRaycaster.cs
+++ Assets/Scripts/RecursiveRaycaster.cs
@@ -1,42 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using AssemblyCSharp;
 
 public class RecursiveRaycaster : MonoBehaviour {
 	// This script is used to recursively cast rays in any and all directions
 	// up to a specified number if times, each time a ray hits a solid object it 
 	// will spawn a reflected ray.
 
-	public int num_rays;		// to be equally dispersed radially
-	public int num_recursions;	// number of bounces
+	public int num_rays = 8;			// to be equally dispersed radially
+	public int num_recursions = 2;		// number of bounces
+	public float line_display_time = 10.0f; // in seconds
+	
+	private List<Tuple<Vector3, int>> rays;	// contains the info for each ray, its current direction and level of recusion.
 
 	// Use this for initialization
 	void Start () {
-	
+		// angle displacement between succesive rays
+		float angle = 2.0f * Mathf.PI / 8;
+		Vector3 dir = new Vector3(0.0f, 0.0f, 1.0f);
+		Quaternion rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
+
+		rays = new List<Tuple<Vector3, int>>();
+		for (int i = 0; i < num_rays; i++) {
+			Tuple<Vector3, int> temp_ray = new Tuple<Vector3, int>(dir, i);
+			rays.Add(temp_ray);
+			// rotate the dir for the next ray
+			dir = rotation * dir;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 dir = new Vector3 (0, 0, -1);
+
+	// This function should be called on button press to cast a single a ray from the sound
+	// source, this function draws the required lines.
+	// This function should recurse if encountering an empty tile and return when it either
+	// fails to encounter a tile or hits a solid object.
+	public void CastSingleRay(int ray_index){
+		// create ray and hit objects
+		Vector3 dir = rays[ray_index].First;
 		Ray ray = new Ray(this.transform.position, dir);
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, Mathf.Infinity) == true){
+		bool recurse = true;	// set to false when ray hits a solid tile or nothing.
+		// Fire the ray, and detect what it hits
+		while (recurse == true){
+			recurse = Physics.Raycast(ray, out hit, Mathf.Infinity);
+			// draw the ray
 			Debug.DrawRay(this.transform.position, hit.point - this.transform.position, Color.red, 6.0f);
-			Debug.Log ("hit");
-			if(hit.collider.CompareTag("Solid")){
+			// if the ray is solid reflect the ray and increment the reflection
+			if (!recurse){
+				break;
+			}
+			else if(hit.collider.CompareTag("Solid")){
 				Debug.Log("hit solid object");
-				//hit.collider.gameObject.GetComponent<SoundOcclusionElement>().isHit();
-				Vector3 new_dir = dir - 2.0f * Vector3.Dot(dir, hit.normal) * hit.normal;
-				ray = new Ray(hit.point, new_dir);
+				// calc the reflected ray direction and increment the recurse number
+				rays[ray_index].First = dir - 2.0f * Vector3.Dot(dir, hit.normal) * hit.normal;
+				rays[ray_index].Second += 1;
+				// draw a debug line to show that reflect has worked
+				ray = new Ray(hit.point, rays[ray_index].First);
 				Vector3 point = hit.point;
-				Debug.DrawRay(point, new_dir, Color.green, 6.0f);
-				Physics.Raycast(ray, out hit, Mathf.Infinity);
-				Debug.DrawRay(point, hit.point - point, Color.blue, 6.0f);
-				Debug.Log ("hit");
+				Debug.DrawRay(point, rays[ray_index].First, Color.green, 6.0f);
+				recurse = false;
 			}
 			else if (hit.collider.CompareTag("Empty")){
+				// If the object is empty fire another ray in the same direction and turn the 
+				// layer off to avoid raycast hits
 				Debug.Log("hit empty object");
 				hit.collider.gameObject.GetComponent<SoundOcclusionElement>().isHit();
 			}
 		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
 	}
 }

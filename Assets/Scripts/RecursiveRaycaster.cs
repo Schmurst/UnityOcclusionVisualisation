7e07bd1341b7eb0d9337ac1f2249aaab63dd00ce
Sam <samhayhurst@gmail.com>
2015-05-28 14:42:30 +0100

    Add tile prefabs and TileController Script.

--- Assets/Scripts/RecursiveRaycaster.cs
+++ Assets/Scripts/RecursiveRaycaster.cs
@@ -12,7 +12,7 @@ public class RecursiveRaycaster : MonoBehaviour {
 	public int num_recursions = 2;		// number of bounces
 	public float line_display_time = 10.0f; // in seconds
 	
-	private List<Tuple<Vector3, int>> rays;	// contains the info for each ray, its current direction and level of recusion.
+	private List<Tuple<Vector3, Vector3>> rays;	// contains the info for each ray, its current direction and level of recusion.
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +21,9 @@ public class RecursiveRaycaster : MonoBehaviour {
 		Vector3 dir = new Vector3(0.0f, 0.0f, 1.0f);
 		Quaternion rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
 
-		rays = new List<Tuple<Vector3, int>>();
+		rays = new List<Tuple<Vector3, Vector3>>();
 		for (int i = 0; i < num_rays; i++) {
-			Tuple<Vector3, int> temp_ray = new Tuple<Vector3, int>(dir, i);
+			Tuple<Vector3, Vector3> temp_ray = new Tuple<Vector3, Vector3>(dir, this.transform.position);
 			rays.Add(temp_ray);
 			// rotate the dir for the next ray
 			dir = rotation * dir;
@@ -37,26 +37,27 @@ public class RecursiveRaycaster : MonoBehaviour {
 	public void CastSingleRay(int ray_index){
 		// create ray and hit objects
 		Vector3 dir = rays[ray_index].First;
-		Ray ray = new Ray(this.transform.position, dir);
+		Vector3 pos = rays[ray_index].Second;
+		Ray ray = new Ray(pos, dir);
 		RaycastHit hit;
 		bool recurse = true;	// set to false when ray hits a solid tile or nothing.
 		// Fire the ray, and detect what it hits
 		while (recurse == true){
 			recurse = Physics.Raycast(ray, out hit, Mathf.Infinity);
 			// draw the ray
-			Debug.DrawRay(this.transform.position, hit.point - this.transform.position, Color.red, 6.0f);
+			Debug.DrawRay(pos, hit.point - pos, Color.red, 6.0f);
 			// if the ray is solid reflect the ray and increment the reflection
 			if (!recurse){
 				break;
 			}
 			else if(hit.collider.CompareTag("Solid")){
 				Debug.Log("hit solid object");
+				Vector3 point = hit.point;
 				// calc the reflected ray direction and increment the recurse number
 				rays[ray_index].First = dir - 2.0f * Vector3.Dot(dir, hit.normal) * hit.normal;
-				rays[ray_index].Second += 1;
+				rays[ray_index].Second = point;
 				// draw a debug line to show that reflect has worked
 				ray = new Ray(hit.point, rays[ray_index].First);
-				Vector3 point = hit.point;
 				Debug.DrawRay(point, rays[ray_index].First, Color.green, 6.0f);
 				recurse = false;
 			}

8e4a6d390a4357cd291493e31d9d12d07189f324
Sam <samhayhurst@gmail.com>
2015-05-28 15:32:38 +0100

    TileController now generates a new level layout on start.

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TileController : MonoBehaviour {
 	// This class will create the tiled level for the sound source to
 	// raycast within.
 	public GameObject Empty_tile;
 	public GameObject Solid_tile;
-	public int grid_size = 20;
+	public int grid_size = 10;
+
+	private List<GameObject> Tiles;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		Tiles = new List<GameObject>();
+		GameObject tile;
+		// layout the tilse
+		for (int j = - grid_size; j < grid_size; j++) {
+			for (int i = -grid_size; i < grid_size; i++) {
+				if (Random.value < 0.9f){
+					tile = GameObject.Instantiate<GameObject>(Empty_tile);
+				}
+				else{
+					tile = GameObject.Instantiate<GameObject>(Solid_tile);
+				}
+				tile.transform.SetParent(this.transform, false);
+				tile.transform.Translate( new Vector3(i, 0.0f, j));
+				Tiles.Add(tile);
+			}
+		}
 	}
 }

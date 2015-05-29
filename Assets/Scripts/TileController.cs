021ecaf25456b88702e0d0d78f7691f0d6393547
Sam <samhayhurst@gmail.com>
2015-05-29 15:58:34 +0100

    Fix bug where the TileController would spawn a solid tile under the soundSource.

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -44,7 +44,7 @@ public class TileController : MonoBehaviour {
 		// layout the tilse
 		for (int j = - grid_size; j < grid_size; j++) {
 			for (int i = -grid_size; i < grid_size; i++) {
-				if (Random.value < 0.9f || (i == grid_size * 0.5f && j == grid_size * 0.5f)){
+				if (Random.value < 0.9f || (i == 0 && j == 0)){
 					tile = GameObject.Instantiate<GameObject>(Empty_tile);
 				}
 				else{
